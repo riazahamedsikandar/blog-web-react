656711ce4873da5824ef8475f7de75f5eaaebfdf
riaz ahamed sikandar <riazahamedsikandar@gmail.com>
2023-04-24 01:23:38 +0530

    react

---
+++ blog-web/src/components/AddArticle.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function AddArticle() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
---
+++ blog-web/src/firebaseConfig.js
@@ -0,0 +1,20 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCwldBRkq-Y4CCD6yEcfGB7zFvmrj5YNk8",
+  authDomain: "blog-website-using-react.firebaseapp.com",
+  projectId: "blog-website-using-react",
+  storageBucket: "blog-website-using-react.appspot.com",
+  messagingSenderId: "305690863387",
+  appId: "1:305690863387:web:eba32b4fa7464d662cd5be",
+  measurementId: "G-PGNCF46V6Y",
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const storage = getStorage(app);
+export const db = getFirestore(app);
